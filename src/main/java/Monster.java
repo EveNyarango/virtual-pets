104489f95b2d1ddcb82740cb769c3ff77c2c56dd
EveNyarango <evenyarango@gmail.com>
2020-12-02 11:42:41 +0300

    added more test and functionality

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -1,17 +1,72 @@
+import java.util.Objects;
+import org.sql2o.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Monster {
     private String name;
+    private int personId;
     private int id;
 
-    public Monster(String name, int id) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Monster monster = (Monster) o;
+        return personId == monster.personId &&
+                name.equals(monster.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, personId);
+    }
+
+
+    public Monster(String name, int personId) {
         this.name = name;
-        this.id = id;
+        this.personId = personId;
     }
 
     public String getName() {
         return name;
     }
 
+    public int getPersonId() {
+        return personId;
+    }
+
     public int getId() {
         return id;
     }
+
+    public void save() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO monsters (name, personid) VALUES (:name, :personId)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("personId", this.personId)
+                    .executeUpdate()
+                    .getKey();
+        }
+
+    }
+//all() method
+    public static List<Monster> all() {
+        String sql = "SELECT * FROM monsters";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Monster.class);
+        }
+    }
+//find
+    public static Monster find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM monsters where id=:id";
+            Monster monster = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Monster.class);
+            return monster;
+        }
+    }
+
 }
--- src/test/java/MonsterTest.java
+++ src/test/java/MonsterTest.java
@@ -12,5 +12,54 @@ public class MonsterTest {
         Monster testMonster = new Monster("Bubbles", 1);
         assertEquals(true, testMonster instanceof Monster);
     }
+    @Test
+    public void Monster_instantiatesWithPersonId_int() {
+        Monster testMonster = new Monster("Bubbles", 1);
+        assertEquals(1, testMonster.getPersonId());
+    }
+    @Test
+    public void equals_returnsTrueIfNameAndPersonIdAreSame_true() {
+        Monster testMonster = new Monster("Bubbles", 1);
+        Monster anotherMonster = new Monster("Bubbles", 1);
+        assertTrue(testMonster.equals(anotherMonster));
+
+    }
+//    save
+    @Test
+    public void save_returnsTrueIfDescriptionsAretheSame() {
+        Monster testMonster = new Monster("Bubbles", 1);
+        testMonster.save();
+        assertTrue(Monster.all().get(0).equals(testMonster));
+    }
+
+    @Test
+    public void all_returnsAllInstancesOfMonster_true() {
+        Monster firstMonster = new Monster("Bubbles", 1);
+        firstMonster.save();
+        Monster secondMonster = new Monster("Spud", 1);
+        secondMonster.save();
+        assertEquals(true, Monster.all().get(0).equals(firstMonster));
+        assertEquals(true, Monster.all().get(1).equals(secondMonster));
+    }
+
+    //    finding
+    @Test
+    public void find_returnsMonsterWithSameId_secondMonster() {
+        Monster firstMonster = new Monster("Bubbles", 1);
+        firstMonster.save();
+        Monster secondMonster = new Monster("Spud", 3);
+        secondMonster.save();
+        assertEquals(Monster.find(secondMonster.getId()), secondMonster);
+    }
+//one-to-many
+    @Test
+    public void save_savesPersonIdIntoDB_true() {
+        Person testPerson = new Person("Henry", "henry@henry.com");
+        testPerson.save();
+        Monster testMonster = new Monster("Bubbles", testPerson.getId());
+        testMonster.save();
+        Monster savedMonster = Monster.find(testMonster.getId());
+        assertEquals(savedMonster.getPersonId(), testPerson.getId());
+    }
 
 }
