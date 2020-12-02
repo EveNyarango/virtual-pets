a39f39706b9e9eba78d12c1ab8ad8a529ac4ebe7
EveNyarango <evenyarango@gmail.com>
2020-12-02 10:28:22 +0300

    add database in the terminal and adding monstertest class

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -1,2 +1,17 @@
 public class Monster {
+    private String name;
+    private int id;
+
+    public Monster(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
